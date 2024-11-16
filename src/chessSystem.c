ee6dc01b20726d676dac1497cb1948a6c875fa9f
Baraa Egbaria <mohammadegbaria@Mohammads-MacBook-Pro.local>
2024-11-16 05:24:25 +0200

    write levels to file

--- src/chessSystem.c
+++ src/chessSystem.c
@@ -11,6 +11,10 @@
 #include "../includes/Tournament.h"
 #include "../includes/chessSystem.h"
 
+#define WINS_FACTOR 6
+#define LOSSES_FACTOR 10
+#define DRAWS_FACTOR 2
+
 struct chess_system_t
 {
     Map tournaments;
@@ -81,22 +85,63 @@ ChessResult chessRemoveTournament(ChessSystem chess, int tournamentID)
 {
     if(!chess)                       return CHESS_NULL_ARGUMENT;
     else if(tournamentID <= 0 )      return CHESS_INVALID_ID;
-    else if(mapContains(chess->tournaments, &tournamentID) == false) 
+    else if(mapContains(chess->tournaments, &tournamentID) == false)
         return CHESS_TOURNAMENT_NOT_EXIST;
 
     Tournament toDelete = mapGet(chess->tournaments, &tournamentID);
-    Map gamesList = tournamentGetGamesList(toDelete);
-    MAP_FOREACH(const char*, currentGameKey, gamesList)
-    {
-        Game game = mapGet(gamesList, (MapKeyElement) currentGameKey);
-        int firstPlayerID = GameGetPlayer1ID(game);
-        int secondPlayerID = GameGetPlayer2ID(game);
-        Player player1 = mapGet(chess->players, &firstPlayerID);
-        Player player2 = mapGet(chess->players, &secondPlayerID);
-        INFO status = REMOVE;
-        updatePlayersInfo(player1, player2, GameGetWinner(game), GameGetPlayTime(game), status);
-        freeChar((MapKeyElement) currentGameKey);
-    }
+    
     mapRemove(chess->tournaments, &tournamentID);
+    return CHESS_SUCCESS;
+
+
+double chessCalculateAveragePlayTime(ChessSystem chess, int playerID, ChessResult* ChessResult)
+{
+    if(! chess|| !ChessResult) {
+        *ChessResult = CHESS_NULL_ARGUMENT;
+        return 0;
+    }
+    if(playerID <= 0) {
+        *ChessResult= CHESS_INVALID_ID;
+        return 0;
+    }
+
+    Player currPlayer = mapGet(chess->players,&playerID);
+    if(!currPlayer || GetPlayerStatus(currPlayer) == DELETED)
+    {
+        *ChessResult= CHESS_PLAYER_NOT_EXIST;
+        return 0;
+    }
+    double result = (double)(PlayerGetTotalPlayTime(currPlayer)) / PlayerGetNumOfPlayedGames(currPlayer);
+    *ChessResult = CHESS_SUCCESS;
+    return result;
+}
+
+static double calculatePlayerLevel(Player player)
+{
+    double sum = (PlayergetWinsNum(player)*WINS_FACTOR) - (PlayergetLossesNum(player)*LOSSES_FACTOR)
+            + (PlayergetDrawsNum(player)*DRAWS_FACTOR);
+    return sum / (double) PlayerGetNumOfPlayedGames(player) ;
+}
+
+ChessResult chessSavePlayersLevels(ChessSystem chess, FILE* file)
+{
+    if(!chess || !file) return CHESS_NULL_ARGUMENT;
+
+    // Map playersCopy = mapCreate(PlayerCopy, copyIntkey,
+    //                              PlayerDestroy, freeIntKey, Intkeycompare);
+    int maxLevelID = mapGetFirst(chess->players);
+    int maxLevel = calculatePlayerLevel(mapGet(maxLevelID));
+    MAP_FOREACH(int*, playerID, chess->players)
+    {
+        Player player = mapGet(chess->players, playerID);
+        if (PlayerGetPlayingStatus(player) != true || PlayerGetNumOfPlayedGames(player) == 0)
+            // mapPut(playersCopy, currPlayer, player);
+            continue;
+        int tempLevel = calculatePlayerLevel(player);
+        maxLevel = tempLevel > maxLevel ? tempLevel : maxLevel;
+        maxLevelID = tempLevel > maxLevel > *playerID : maxLevelID;
+        freeIntKey(playerID);
+    }
+    fprintf(file, "%d %.2f\n", maxLevelID, maxLevel);
     return CHESS_SUCCESS;
 }
